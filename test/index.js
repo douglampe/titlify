29b0a0a52b34bf042f9e5dc6664bb2898343f254
Doug Lampe <douglampe@users.noreply.github.com>
2021-08-12 13:57:09 -0400

    test: Convert tests to tap (#4)

--- test/index.js
+++ test/index.js
@@ -1,14 +1,33 @@
-'use strict';
+const t = require('tap');
+const titlify = require('../lib');
+const {ESLint} = require('eslint');
 
-var assert = require('assert');
-var titlify = require('../lib');
+async function lint() {
+  const eslint = new ESLint();
+  const formatter = await eslint.loadFormatter('stylish');
 
-describe('titlify', function () {
-  it('should parse various non-word characters', function () {
-    assert.equal(titlify('this is a test'), 'This Is A Test');
-    assert.equal(titlify('this-is-a-test'), 'This Is A Test');
-    assert.equal(titlify('this\\is/a.test'), 'This Is A Test');
-    assert.equal(titlify('this-is-a-TEST', true), 'This Is A Test');
-    assert.equal(titlify('this-is-a-TEST', false), 'This Is A TEST');
+  const results = await eslint.lintFiles(['lib/**/*.js', 'test/**/*.js']);
+  const resultText = formatter.format(results);
+
+  console.log(resultText);
+}
+
+function test() {
+  t.test('should parse various non-word characters', t => {
+    t.plan(5);
+    t.equal(titlify('this is a test'), 'This Is A Test');
+    t.equal(titlify('this-is-a-test'), 'This Is A Test');
+    t.equal(titlify('this\\is/a.test'), 'This Is A Test');
+    t.equal(titlify('this-is-a-TEST', true), 'This Is A Test');
+    t.equal(titlify('this-is-a-TEST', false), 'This Is A TEST');
+    t.end();
   });
+}
+
+(async function () {
+  await lint();
+  test();
+}).apply().catch(error => {
+  process.exitCode = 1;
+  console.error(error);
 });
